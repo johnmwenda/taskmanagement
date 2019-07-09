b30edfe03e31947fd0c7b1001909680fdc2e3996
John Mwenda <jmwenda56@gmail.com>
2019-07-09 20:34:59 +0300

    create department app models

--- tasksystem/departments/models.py
+++ tasksystem/departments/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class Department(models.Model):
     name = models.CharField(max_length=100)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, blank=True, null=True)
