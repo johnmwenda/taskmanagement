d3f84d034665338c0267af59a8fc9ac25ed4292f
john <jmwenda56@gmail.com>
2019-08-02 13:07:25 +0300

    start using docker
    1. use gt mv to move files into an app directory
    2. update .gitignore to reflect changes
    3. create necessary docker configuration
    3. update settings.py to use new postgres docker image

---
+++ app/tasksystem/accounts/models.py
@@ -0,0 +1,56 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from rest_framework.authtoken.models import Token
+
+from tasksystem.accounts.managers import UserManager
+from tasksystem.departments.models import Department
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    date_joined = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=255, null=False, blank=False, unique=True)
+    first_name = models.CharField(max_length=255, null=False, blank=False) 
+    last_name = models.CharField(max_length=255, null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    is_member = models.BooleanField(default=False)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+    follows = models.ManyToManyField(
+        'self',
+        related_name='followed_by',
+        symmetrical=False,
+        blank=True,
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        app_label = 'accounts'
+
+    def __str__(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.first_name
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def save(self, *args, **kwargs):
+        # if no password is provided generate a password, to avoid passwordless access
+        if not self.password:
+            self.password = str(uuid.uuid4()).replace('-', '')
+        super(User, self).save(*args, **kwargs)
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
---
+++ app/tasksystem/departments/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    department = models.ForeignKey(Department, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.department}'
